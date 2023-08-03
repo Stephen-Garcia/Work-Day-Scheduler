f61c7c5e55d1664f133fdf56f8ce3f055478410d
Stephen Garcia <garciastephen5280@gmail.com>
2023-08-03 17:22:29 -0600

    added javascript for getting current hour, loop over time blocks, and blocks to check time and add classes for background

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -8,4 +8,27 @@ $(document).ready(function () {
     localStorage.setItem(time, text);
   })
 
+  function timeTracker() {
+    var timeNow = moment().hour();
+    $(".time-block").each(function () {
+      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+      if (blockTime < timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+      }
+      else if (blockTime === timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+      }
+      else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+      }
+    })
+  }
+
+  
 });
