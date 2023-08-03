7d2dfbeb07b222ed0d74b8696bdda1e31942c4a6
Stephen Garcia <garciastephen5280@gmail.com>
2023-08-03 17:29:31 -0600

    added javascript for getting item from local storage

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, text);
-  })
+  });
 
   function timeTracker() {
     var timeNow = moment().hour();
@@ -26,9 +26,20 @@ $(document).ready(function () {
         $(this).removeClass("present");
         $(this).removeClass("past");
         $(this).removeClass("future");
-      }
-    })
-  }
+      };
+    });
+  };
 
-  
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+  timeTracker();
 });
